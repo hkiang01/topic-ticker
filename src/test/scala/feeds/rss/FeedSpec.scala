6990a4cb0603542103a7c95d9362a97dbc2ba006
Harrison Kiang <hkiang01@gmail.com>
2017-07-29 20:10:28 -0400

    more tests on feeds

--- src/test/scala/feeds/rss/FeedSpec.scala
+++ src/test/scala/feeds/rss/FeedSpec.scala
@@ -5,7 +5,7 @@ import org.scalatest.{FunSpec, Matchers}
 
 class FeedSpec extends FunSpec with Matchers {
 
-  describe("feed connection") {
+  describe("single feed connection of a given type") {
     class SampleFeed1 extends Feed {
       override val schema: Schema = Schema(Array(
         SchemaCol("col1", "type1")
@@ -27,7 +27,7 @@ class FeedSpec extends FunSpec with Matchers {
     }
   }
 
-  describe("multiple feed connections") {
+  describe("multiple feed connections of different types") {
     class SampleFeed1 extends Feed {
       override val schema: Schema = Schema(Array(
         SchemaCol("col1", "type1")
@@ -43,16 +43,16 @@ class FeedSpec extends FunSpec with Matchers {
     val sampleFeed1 = new SampleFeed1
     val sampleFeed2 = new SampleFeed2
 
-    it("multiple feed connections should be valid") {
+    it("multiple feed connections of different types should be valid") {
       val sampleFeedConnection1 = sampleFeed1.connection
       val sampleFeedConnection2 = sampleFeed2.connection
-      sampleFeedConnection1.isValid(200) should be (true)
-      sampleFeedConnection2.isValid(200) should be (true)
+      sampleFeedConnection1.isValid(500) should be (true)
+      sampleFeedConnection2.isValid(500) should be (true)
       sampleFeedConnection1.close()
       sampleFeedConnection2.close()
     }
 
-    it("multiple feed connections should be closeable") {
+    it("multiple feed connections of different types should be closeable") {
       val sampleFeedConnection1 = sampleFeed1.connection
       val sampleFeedConnection2 = sampleFeed2.connection
       sampleFeedConnection1.close()
@@ -60,7 +60,25 @@ class FeedSpec extends FunSpec with Matchers {
       sampleFeedConnection2.close()
       sampleFeedConnection2.isClosed should be (true)
     }
+  }
 
+  describe("multiple feed instances of same type") {
+    class SampleFeed1 extends Feed {
+      override val schema: Schema = Schema(Array(
+        SchemaCol("col1", "type1")
+      ))
+    }
 
+    val sampleFeed1 = new SampleFeed1
+    val anotherSampleFeed1 = new SampleFeed1
+
+    it("multiple feed instances of same type should be able to have valid connections") {
+      val sampleFeedConnection1 = sampleFeed1.connection
+      val anotherSampleFeedConnection1 = anotherSampleFeed1.connection
+      sampleFeedConnection1.isValid(500) should be (true)
+      anotherSampleFeedConnection1.isValid(500) should be (true)
+      sampleFeedConnection1.close()
+      anotherSampleFeedConnection1.close()
+    }
   }
 }
